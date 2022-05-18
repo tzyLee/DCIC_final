3f5dadc14a2ec40fdc6888c4dcd213a614d778f9
tzyLee <b06901145@ntu.edu.tw>
2022-05-18 17:21:18 +0800

    Rotate 180 degree

--- CORDIC.m
+++ CORDIC.m
@@ -3,19 +3,17 @@ function [x_out, y_out, z_out] = CORDIC(x, y, z, mode, iter)
     y_inv = 0;
 
     gain = 1;
-    if mode == "rotation"
-        swap = 0;
-        if z > pi/2 && z <= pi
-            z = z - pi/2;
-            x_inv = 1;
-            swap = 1;
-        elseif z < -pi/2 && z >= -pi
-            z = z + pi/2;
-            y_inv = 1;
-            swap = 1;
-        elseif z > pi/2 || z < -pi/2
-            error("z is not in convergence range");
+    if z < -pi/2 || z > pi/2
+        if z < 0
+            z = z + pi;
+        else
+            z = z - pi;
         end
+        x_inv = 1;
+        y_inv = 1;
+    end
+
+    if mode == "rotation"
         if abs(z) > 1.7433
             error("z is not in convergence range");
         end
@@ -39,12 +37,6 @@ function [x_out, y_out, z_out] = CORDIC(x, y, z, mode, iter)
             z = z_n;
         end
 
-        if swap
-            temp = x;
-            x = y;
-            y = temp;
-        end
-
         if x_inv
             x = -x;
         end
@@ -55,13 +47,12 @@ function [x_out, y_out, z_out] = CORDIC(x, y, z, mode, iter)
     elseif mode == "vectoring"
         if x < 0
             if y < 0
-                swap = 1;
+                z = -pi;
             else
-                swap = 0;
+                z = pi;
             end
             x = -x;
             y = -y;
-            x_inv = 1;
         end
         if atan(y/x) > 1.7433
             error("tan-1(y/x) is not in convergence range");
@@ -84,14 +75,6 @@ function [x_out, y_out, z_out] = CORDIC(x, y, z, mode, iter)
             x = x_n;
             y = y_n;
             z = z_n;
-        end
-        
-        if x_inv
-            if swap
-                z = -pi+z;
-            else
-                z = pi+z;
-            end
         end
     else
         error("Unknown operation mode %s", mode);
--- CORDIC_test.m
+++ CORDIC_test.m
@@ -1,26 +1,26 @@
 clc;
 
 %% Rotation mode
-% xx = [-2; 3];
-% angle = -2*pi/3;
-% rot = [cos(angle) -sin(angle); sin(angle) cos(angle)];
-% out = rot * xx;
-% 
-% [x, y, z] = CORDIC(xx(1), xx(2), angle, 'rotation', 30);
-% 
-% fprintf("Rotation mode test:\n");
-% fprintf("Expected: %f %f\n", out);
-% fprintf("Actual: %f %f\n\n", x, y);
+xx = [2; -3];
+angle = -2*pi/3;
+rot = [cos(angle) -sin(angle); sin(angle) cos(angle)];
+out = rot * xx;
+
+[x, y, z] = CORDIC(xx(1), xx(2), angle, 'rotation', 30);
+
+fprintf("Rotation mode test:\n");
+fprintf("Expected: %f %f\n", out);
+fprintf("Actual: %f %f\n\n", x, y);
 
 
 %% Vectoring mode
-% xx = [-5; 4];
-% 
-% [x, y, z] = CORDIC(xx(1), xx(2), 0, 'vectoring', 12);
-% 
-% fprintf("Vectoring mode test:\n");
-% fprintf("Expected: %f %f\n", vecnorm(xx), atan(xx(2)/xx(1)));
-% fprintf("Actual: %f %f\n\n", x, z);
+xx = [-5; 4];
+
+[x, y, z] = CORDIC(xx(1), xx(2), 0, 'vectoring', 12);
+
+fprintf("Vectoring mode test:\n");
+fprintf("Expected: %f %f\n", vecnorm(xx), atan(xx(2)/xx(1)));
+fprintf("Actual: %f %f\n\n", x, z);
 
 
 %% QR decomposition
@@ -34,14 +34,10 @@ H = (randn(4, 4) + 1j.*randn(4, 4)) ./ sqrt(2);
 
 fprintf("Expected:\n");
 disp("===========================");
-disp(Q');
-disp(R);
 disp(Q*R);
 disp("===========================");
 fprintf("Actual:\n");
 disp("===========================");
-disp(QC');
-disp(RC);
 disp(QC*RC);
 disp("===========================");
 
@@ -49,4 +45,5 @@ fprintf("Error of R:\n");
 disp(abs(R-RC));
 fprintf("Error of Q:\n");
 disp(abs(Q-QC));
+fprintf("Error of Q*R:\n");
 disp(abs(Q*R - QC*RC));
