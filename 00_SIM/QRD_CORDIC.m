3fee99bae521564ba1a030e7085ec9a930c0a372
tzyLee <r10943024@ntu.edu.tw>
2022-06-02 15:02:21 +0800

    Synthesize 8 cycle design (simplify mult)

--- 00_SIM/QRD_CORDIC.m
+++ 00_SIM/QRD_CORDIC.m
@@ -9,9 +9,9 @@ for col = 1:s(2)-1
         b = R(row, col);
 
         % calculate 3 angles
-        [abs_a, ~, ang_a] = CORDIC_Q(real(a), imag(a), 0, 'vectoring', iter);
-        [abs_b, ~, ang_b] = CORDIC_Q(real(b), imag(b), 0, 'vectoring', iter);
-        [norm_ab, ~, ang_1] = CORDIC_Q(abs_a, abs_b, 0, 'vectoring', iter);
+        [abs_a, ~, ang_a] = CORDIC_Q_factor_scale(real(a), imag(a), 0, 'vectoring', iter);
+        [abs_b, ~, ang_b] = CORDIC_Q_factor_scale(real(b), imag(b), 0, 'vectoring', iter);
+        [norm_ab, ~, ang_1] = CORDIC_Q_factor_scale(abs_a, abs_b, 0, 'vectoring', iter);
 
         R(col, col) = norm_ab;
         R(row, col) = 0;
@@ -20,11 +20,11 @@ for col = 1:s(2)-1
             c = R(col, col2);
             d = R(row, col2);
 
-            [s_cr, s_ci, ~] = CORDIC_Q(real(c), imag(c), -ang_a, 'rotation', iter);
-            [s_dr, s_di, ~] = CORDIC_Q(real(d), imag(d), -ang_b, 'rotation', iter);
+            [s_cr, s_ci, ~] = CORDIC_Q_factor_scale(real(c), imag(c), -ang_a, 'rotation', iter);
+            [s_dr, s_di, ~] = CORDIC_Q_factor_scale(real(d), imag(d), -ang_b, 'rotation', iter);
     
-            [x_r, y_r, ~] = CORDIC_Q(s_cr, s_dr, -ang_1, 'rotation', iter);
-            [x_i, y_i, ~] = CORDIC_Q(s_ci, s_di, -ang_1, 'rotation', iter);
+            [x_r, y_r, ~] = CORDIC_Q_factor_scale(s_cr, s_dr, -ang_1, 'rotation', iter);
+            [x_i, y_i, ~] = CORDIC_Q_factor_scale(s_ci, s_di, -ang_1, 'rotation', iter);
 
             R(col, col2) = x_r+1j*x_i;
             R(row, col2) = y_r+1j*y_i;
@@ -34,11 +34,11 @@ for col = 1:s(2)-1
             c = QH(col, col2);
             d = QH(row, col2);
 
-            [s_cr, s_ci, ~] = CORDIC_Q(real(c), imag(c), -ang_a, 'rotation', iter);
-            [s_dr, s_di, ~] = CORDIC_Q(real(d), imag(d), -ang_b, 'rotation', iter);
+            [s_cr, s_ci, ~] = CORDIC_Q_factor_scale(real(c), imag(c), -ang_a, 'rotation', iter);
+            [s_dr, s_di, ~] = CORDIC_Q_factor_scale(real(d), imag(d), -ang_b, 'rotation', iter);
     
-            [x_r, y_r, ~] = CORDIC_Q(s_cr, s_dr, -ang_1, 'rotation', iter);
-            [x_i, y_i, ~] = CORDIC_Q(s_ci, s_di, -ang_1, 'rotation', iter);
+            [x_r, y_r, ~] = CORDIC_Q_factor_scale(s_cr, s_dr, -ang_1, 'rotation', iter);
+            [x_i, y_i, ~] = CORDIC_Q_factor_scale(s_ci, s_di, -ang_1, 'rotation', iter);
 
             QH(col, col2) = x_r+1j*x_i;
             QH(row, col2) = y_r+1j*y_i;
